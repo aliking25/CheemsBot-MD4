a7f5e66e8e867122c23beb7b2bdacf3ab4ec7e10
aliking25 <114737276+aliking25@users.noreply.github.com>
2022-11-04 22:07:21 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,19 +54,19 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['923252752761'] //ur owner number
+global.ownername = "ALI-KING" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['923252752761']
+global.ownertag = '923252752761' //ur tag number
+global.botname = 'ALI-KING V4' //ur bot name
+global.linkz = "https://chat.whatsapp.com/IHc6Ga3pHWdDLQle2v7rx3" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chat.whatsapp.com/IHc6Ga3pHWdDLQle2v7rx3" //ur website to be displayed
+global.botscript = 'https://chat.whatsapp.com/IHc6Ga3pHWdDLQle2v7rx3' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['923252752761'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
